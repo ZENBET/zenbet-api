18d2a8c752e8ae6506117c5e233024cc043ae2a0
samuel viaña <svianal1@upao.edu.pe>
2023-05-17 14:23:57 -0500

    feat: Implementar la funcionalidad de crear apostador sin sobrescribir DNI

--- zenbet-api/zenbet-api/src/main/java/com/zenbet/zenbetapi/service/impl/ApostadorServiceImpl.java
+++ zenbet-api/zenbet-api/src/main/java/com/zenbet/zenbetapi/service/impl/ApostadorServiceImpl.java
@@ -15,6 +15,8 @@ public class ApostadorServiceImpl implements ApostadorService {
 
     @Override
     public Apostador crearApostador(Apostador apostador) {
-        return apostadorRepository.save(apostador);
+        if(apostadorRepository.findById(apostador.getDni()).isEmpty())
+            return apostadorRepository.save(apostador);
+        else return null;
     }
 }
