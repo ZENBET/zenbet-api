7e09cced6adee68ccce42fa02245f270c9cb35db
Sriosc4 <sriosc4@upao.edu.pe>
2023-05-18 02:18:07 -0500

    feat: Implementar la funcionalidad listar equipos

--- zenbet-api/zenbet-api/src/main/java/com/zenbet/zenbetapi/service/impl/EquipoServiceImpl.java
+++ zenbet-api/zenbet-api/src/main/java/com/zenbet/zenbetapi/service/impl/EquipoServiceImpl.java
@@ -1,4 +1,19 @@
 package com.zenbet.zenbetapi.service.impl;
 
-public class EquipoServiceImpl {
+import com.zenbet.zenbetapi.domain.Equipo;
+import com.zenbet.zenbetapi.repository.EquipoRepository;
+import com.zenbet.zenbetapi.service.EquipoService;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+@Service
+public class EquipoServiceImpl implements EquipoService {
+    private final EquipoRepository equipoRepository;
+
+    public EquipoServiceImpl(EquipoRepository equipoRepository) { this.equipoRepository = equipoRepository; }
+
+    @Override
+    public List<Equipo> listarAllEquipos() { return equipoRepository.findAll(); }
+
 }
